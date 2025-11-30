9661d4048c8dff39a4880b8dfd979d21543b9f3f
YURESSA <goshrenko@gmail.com>
2025-11-30 22:36:53 +0500

    Тест

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-# Загружаем переменные из .env
-export $(grep -v '^#' /app/virtualization/frontend/.env | xargs)
+set -e
+
+# Проверяем, что BACKEND_URL есть в окружении
+: "${BACKEND_URL:?Need to set BACKEND_URL}"
 
 # Генерируем Nginx конфиг из шаблона
-envsubst '$BACKEND_URL' < /app/virtualization/frontend/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_URL}' < /app/nginx.template.conf > /etc/nginx/conf.d/default.conf
 
 # Запускаем Nginx
 exec nginx -g 'daemon off;'
